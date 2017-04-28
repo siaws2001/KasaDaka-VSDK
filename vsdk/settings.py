de73ff4cf43da12852413c993afd2f175a6e4b36
siaws2001 <siaws2001@hotmail.com>
2017-04-29 00:36:52 +0200

    added debugging

--- vsdk/settings.py
+++ vsdk/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'tk2(l(00&kfe7j97j$dvgz&b6r!kk_zbse1(9w*eoc$bcwu773'
 ##########
 #Use True on your local PC, False on Heroku!!
 ########
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
