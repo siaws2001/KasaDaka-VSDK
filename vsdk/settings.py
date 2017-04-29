bf71d91fc4dd5d70d5bf498c65f5f9c581d0a07c
siaws2001 <siaws2001@hotmail.com>
2017-04-30 00:03:21 +0200

    revert back to original setting

--- vsdk/settings.py
+++ vsdk/settings.py
@@ -175,8 +175,8 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-    STATIC_URL = "http://ict4d-vps.andrebaart.nl/django-static/group1/static/"
-    MEDIA_URL = "http://ict4d-vps.andrebaart.nl/django-static/group1/"
+    STATIC_URL = "http://ict4d-vps.andrebaart.nl/django-static/django-files/static/"
+    MEDIA_URL = "http://ict4d-vps.andrebaart.nl/django-static/django-files/"
     STATICFILES_STORAGE = 'vsdk.custom_storages.StaticStorage'
     DEFAULT_FILE_STORAGE ='vsdk.custom_storages.MediaStorage'
 
