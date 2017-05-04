eb1637b5aeeae346da7c5eb4332903f1067fbb31
Nico Griffioen <nicogriffioen@gmail.com>
2017-05-04 12:22:24 +0200

    Log post body

--- vsdk/service_development/views/vse_record.py
+++ vsdk/service_development/views/vse_record.py
@@ -34,6 +34,8 @@ def record(request, element_id, session_id):
     if request.method == "POST":
         session = get_object_or_404(CallSession, pk=session_id)
 
+        print('%s', request.POST)
+
         value = request.POST['recording']
 
         result = lookup_or_create_result(session, record_element.name, value)
